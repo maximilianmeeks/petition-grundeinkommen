00a26177e088605e75e2c13714646e3f786e00a9
Maximilian <maximilian.meeks@gmail.com>
2020-04-21 15:42:02 +0200

    minor commit

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -7,6 +7,7 @@ import DownloadIcon from "./images/download-icon"
 
 
 function MyVerticallyCenteredModal(props) {
+
   return (
     <Modal
       {...props}
@@ -15,23 +16,39 @@ function MyVerticallyCenteredModal(props) {
       centered
     >
       <Modal.Header closeButton>
+          <DownloadIcon/>
       </Modal.Header>
       <Modal.Body>
-        {console.log(props)}
+        
+{/* <Img fluid={data.facebook01.childImageSharp.fluid} alt="Facebook 01"/> */}
+       {/* {console.log(data)}  */}
         <p>hallo</p>
       </Modal.Body>
     </Modal>
   );
 }
 
-function WerdeBotschafter() {
+function WerdeBotschafter(props) {
+    const data = useStaticQuery(graphql `
+    query {
+            facebook01: file(relativePath: { eq: "BGE_post_facebook_1200x627_1.jpg"}) {
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid_noBase64
+                    }
+                }
+            }
+        }
+
+    `)
       const [modalShow, setModalShow] = React.useState(false);
     
       return (
         <Container>
             <Row>
                 <Col xs={6} md={4}>
-                    <Image src="holder.js/171x180" thumbnail onClick={() => setModalShow(true)}/>
+                    {/* <Image src="holder.js/171x180" thumbnail onClick={() => setModalShow(true)}/> */}
+                    <Img fluid={data.facebook01.childImageSharp.fluid} alt="Facebook 01" onClick={() => setModalShow(true)}/>
                     <MyVerticallyCenteredModal
                         show={modalShow}
                         onHide={() => setModalShow(false)}
