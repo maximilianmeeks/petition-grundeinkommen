d91410665ead5cc499e3e7415617740c157c60b3
Maximilian Meeks <maximilianmeeks@MacBook-Pro.fritz.box>
2020-06-26 02:59:24 +0200

    countdown 0 fix

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -2,81 +2,84 @@ import React, { useState, useEffect } from "react"
 import "./countdown.css"
 
 const Countdown = () => {
-  const [countdownDate, setCountdownDate] = useState(new Date('06/30/2020 10:00:00 GMT+0200').getTime());
+  const [countdownDate, setCountdownDate] = useState(
+    new Date("06/30/2020 10:00:00 GMT+0200").getTime()
+  )
   const [state, setState] = useState({
     days: 4,
     hours: 0,
     minutes: 0,
     seconds: 0,
-  });
+  })
 
   useEffect(() => {
-    setInterval(() => setNewTime(), 1000);
-  }, []);
+    setInterval(() => setNewTime(), 1000)
+  }, [])
 
   const setNewTime = () => {
     if (countdownDate) {
-      const currentTime = new Date().getTime();
+      const currentTime = new Date().getTime()
 
-      const distanceToDate = countdownDate - currentTime;
+      const distanceToDate = countdownDate - currentTime
 
-      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
+      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24))
       let hours = Math.floor(
-        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-      );
+        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      )
       let minutes = Math.floor(
-        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60),
-      );
-      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
+        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60)
+      )
+      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000)
 
-      const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-      days = `${days}`;
+      days = `${days}`
       if (numbersToAddZeroTo.includes(hours)) {
-        hours = `0${hours}`;
-      } else if (numbersToAddZeroTo.includes(minutes)) {
-        minutes = `0${minutes}`;
-      } 
+        hours = `0${hours}`
+      }
+      if (numbersToAddZeroTo.includes(minutes)) {
+        minutes = `0${minutes}`
+      }
       if (numbersToAddZeroTo.includes(seconds)) {
         console.log("add 0")
-        seconds = `0${seconds}`;
+        seconds = `0${seconds}`
       }
 
-      setState({ days: days, hours: hours, minutes, seconds });
+      setState({ days: days, hours: hours, minutes, seconds })
     }
-  };
+  }
 
   return (
     <div>
-      <div className='countdown-wrapper row justify-content-center'>
-        <div className='time-section'>
-          <div className='time'>{state.days || '0'}</div>
+      <div className="countdown-wrapper row justify-content-center">
+        <div className="time-section">
+          <div className="time">{state.days || "0"}</div>
           <small className="time-text">Tage</small>
         </div>
-        <div className='time-section'>
-          <div className='time'>:</div>
+        <div className="time-section">
+          <div className="time">:</div>
         </div>
-        <div className='time-section'>
-          <div className='time'>{state.hours || '00'}</div>
+        <div className="time-section">
+          <div className="time">{state.hours || "00"}</div>
           <small className="time-text">Stunden</small>
         </div>
-        <div className='time-section'>
-          <div className='time'>:</div>
+        <div className="time-section">
+          <div className="time">:</div>
         </div>
-        <div className='time-section'>
-          <div className='time'>{state.minutes || '00'}</div>
+        <div className="time-section">
+          <div className="time">{state.minutes || "00"}</div>
           <small className="time-text">Minuten</small>
         </div>
-        <div className='time-section'>
-          <div className='time'>:</div>
+        <div className="time-section">
+          <div className="time">:</div>
         </div>
-        <div className='time-section'>
-          <div className='time'>{state.seconds || '00'}</div>
+        <div className="time-section">
+          <div className="time">{state.seconds || "00"}</div>
           <small className="time-text">Sekunden</small>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Countdown
